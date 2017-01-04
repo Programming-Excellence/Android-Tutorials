b6bad9f378dea01e03d13e4e722eb5cc6ef41a1b
Adeel <adeelsuleman93@hotmail.com>
2017-01-04 14:11:28 +0500

    Addition of Comments
    In order to make the code more understandable some comments write

--- CrudApp/main/java/com/crud/db/crudapp/DbManager.java
+++ CrudApp/main/java/com/crud/db/crudapp/DbManager.java
@@ -11,57 +11,72 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 
 public class DbManager extends SQLiteOpenHelper {
-
+    //database name
     private static String dbName = "PracticeDb";
+    //database version
     private static Integer dbVersion = 1;
-
+    //table create query
     private static String tableQuery = "Create table Users(_id INTEGER PRIMARY KEY AUTOINCREMENT , _username TEXT NOT NULL , _password TEXT NOT NULL , _email TEXT NOT NULL,_alternative_email TEXT NOT NULL)";
 
-
+    //constructor for running create database when application runs
     public DbManager(Context context) {
         super(context, dbName , null, dbVersion);
+        //this lines create database and open table for write purpose (insert data )
         SQLiteDatabase helper = this.getWritableDatabase();
     }
-
-    public int Insertion(String Username, String Password, String Email,String AlternativeEmail){
-
+    
+    //insert method
+    public void Insertion(String Username, String Password, String Email,String AlternativeEmail){
+        //open table for writing purpose edit purpose
         SQLiteDatabase helper = this.getWritableDatabase();
+        //save all values by getting from form
         ContentValues data =new ContentValues();
-        data.put("_username",Username);
+        data.put("_username",Username); //here _username is column name of table 
         data.put("_password",Password);
         data.put("_email",Email);
         data.put("_alternative_email",AlternativeEmail);
+        
+        //run insert builtin method and passed tablename , and values 
+        helper.insert("Users",null,data);
 
-        int id = (int) helper.insert("Users",null,data);
-
-        return id;
     }
-    public Cursor FetchData(String Username, String Password){
+   
+    //fetch record from table method
+    public Cursor GetData(String Username, String Password){
+        //open table for reading purpose and fetch data
         SQLiteDatabase helper = this.getReadableDatabase();
-
-        Cursor reader = helper.query("Users",new String[]{"_id","_username","_password","_email","_alternative_email"},"_username = ? AND _password = ? ",new String[]{Username,Password},null,null,null,null);
+        //cursor is used to check data exists in table or not 
+        //and all data that is fetched will be stored in cursor variable "reader" 
+        //query is the builtin method for fetch data from table
+        String where = "_username=? AND _password =?";
+        Cursor reader = helper.query("Users",new String[]{"_id","_username","_password","_email","_alternative_email"},where,new String[]{Username,Password},null,null,null,null);
+        
         if(reader!=null){
+        
             reader.moveToFirst();
+            
             //moveToFirst() means get data from 1st row , beginnning say data lay ga
+            // but here begin aur last ki baat nahi data wohi aye ga jis ka username aur password matched hoga 
             //moveToLast() means fetch data from last row , akhri data lay kar ayega
         }
 
         return reader;
 
     }
-
-    public int Updation(String id, String Username, String Password, String Email,String AEmail){
-
+ 
+    //update Method 
+    public void Updation(String id, String Username, String Password, String Email,String AEmail){
+        //open table for writable
         SQLiteDatabase helper = this.getWritableDatabase();
         ContentValues data =new ContentValues();
         data.put("_username",Username);
         data.put("_password",Password);
         data.put("_email",Email);
         data.put("_email",AEmail);
-
-
-        int i = (int) helper.update("Users",data,"_id=?",new String[]{id});
-        return i;
+    
+        //update builtn method for database update data , no need to write query 
+        helper.update("Users",data,"_id=?",new String[]{id}); // tablename , data changed value,where argument, where column name
+        
     }
 
     @Override
